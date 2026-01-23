5e3f41055c7ab4e76da4e439fe92887a1c4a53c3
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2026-01-23 22:09:00 +0530

    Refactor code structure for improved readability and maintainability

---
+++ 01_web-development/Week_03_js_101/Practice.js
@@ -0,0 +1,16 @@
+console.log("hi there");
+
+// three ways wee write a variable
+const a = 10;
+let b = 10;
+
+let color = "black";
+let height = 165;
+let likePizza = true;
+
+// operators.
+let sum = 10 + 5;
+let isEqual = 10 === 10;
+
+
+https://brindle-goal-102.notion.site/Course-Selling-API-Coding-Assignment-2f146b36b2e980df9fe4c5856f08d4bc
