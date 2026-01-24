9c2d21a2999871b91f4870bf4307d8db88128c3d
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2026-01-24 12:06:42 +0530

    commiting something basic.

--- 01_web-development/Week_02_js_101/Practice.js
+++ 01_web-development/Week_02_js_101/Practice.js
@@ -9,8 +9,7 @@ let height = 165;
 let likePizza = true;
 
 // operators.
-let sum = 10 + 5;
+let sum = 10 + 6;
 let isEqual = 10 === 10;
-
 
 // https://brindle-goal-102.notion.site/Course-Selling-API-Coding-Assignment-2f146b36b2e980df9fe4c5856f08d4bc
